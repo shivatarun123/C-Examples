e1d367192488183ecf9cf0a518f9ce8815f64a6b
shivatarun123 <bstarun123@gmail.com>
2018-03-02 16:00:28 +0100

    Create placeholder.c

---
+++ placeholder.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// how to print different data types using printf
+void main(){
+	
+	printf("This is line 1");
+	printf("\nThis is line 2\n");
+	printf("%s\n","nikhil"); // to print a string , the placeholder is %s
+	printf("%d\n",10); // to print integer
+	printf("%c\n",'n'); //to print a string
+	printf("%f",2.1); // to print a decimal number  or float
+	
+}
